ccbd3187706f2fb2caa953ce4b32af7bc3906547
Junan Qu <jq77@cornell.edu>
2020-04-18 20:03:08 -0400

    return grid set up

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -10,11 +10,16 @@ net_id = "jq77, zs92, ijp9, mlc294, ns739"
 def search():
     query = request.args.get('search')
     print(query)
+    output_message = ''
     if not query:
         data = []
-        output_message = 'OUTPUT RESULT IS NONE'
-        data = range(10)
     else:
-        output_message = "Your search: " + query
-        data = range(5)
+        data = range(2)
+        output_message_1 = "Your search: " + query
+        if(len(data) >= 3):
+            output_message_2 = 'Here are the top 3 related cases'
+        else:
+            output_message_2 = 'Here are the top {n:.0f} related cases'.format(
+                n=len(data))
+        output_message = output_message_1+' \n '+output_message_2
     return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
