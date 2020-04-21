cd4709ce4de978a4cc305ca787db322eb5adde09
Zack Shine <zackshine1@gmail.com>
2020-04-20 22:44:12 -0400

    used np concatenate in reddit ir retrieval

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -58,8 +58,8 @@ def search():
         print('initialize numpy array')
         tfidf_vec = build_vectorizer(n_feats)
         print("initialize vectorizer")
-        doc_by_vocab = tfidf_vec.fit_transform(
-            [str(data[d]['selftext'])+str(data[d]['title']) for d in data]+[query]).toarray()
+        doc_by_vocab = tfidf_vec.fit_transform(np.concatenate((
+            [str(data[d]['selftext'])+str(data[d]['title']) for d in data],[query]))).toarray()
         print('fit_transform')
         sim_posts = []
         for post_index in range(num_posts):
