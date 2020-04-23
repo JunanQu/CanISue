5aac0498af887afbf9ab32ee3b8d37bad2ec9440
Max Chen <mlc294@cornell.edu>
2020-04-23 16:26:39 -0400

    Create improved results UI (boxed); fix case titles

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -112,7 +112,10 @@ def search():
         #         n=len(res))
 
         # output_message = output_message_1+' \n '+output_message_2
-        caselaw_message = "Historical precedences on '" + query + "':"
-        output_message = "Past discussions on '" + query + "':"
+        caselaw_message = "Historical precedences:"
+        output_message = "Past discussions:"
         print('rendering template..')
-        return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=res[:3], casedata=caseresults, caselaw_message=caselaw_message)
+        return render_template('search.html', name=project_name, netid=net_id, \
+        output_message=output_message, data=res[:3], casedata=caseresults, \
+        caselaw_message=caselaw_message,
+        user_query = query)
