369de5acf17eabb5c49928f1d85c4bd08493b916
Junan Qu <jq77@cornell.edu>
2020-05-01 17:23:29 -0400

    Angular and RQ job queue

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -9,6 +9,16 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 import os
+from flask import Flask, render_template, Response, jsonify
+import time
+
+# ASYNC and LOADING STUFF
+from rq import Queue
+from rq.job import Job
+from worker import conn
+
+q = Queue(connection=conn)
+# END
 print(os.getcwd())
 
 project_name = "Can I Sue?"
@@ -39,22 +49,28 @@ def go_to_about():
     return render_template('about.html')
 
 
-@irsystem.route('/', methods=['GET'])
-def search():
+# @irsystem.route('/loading.html')
+# def go_to_loading():
+#     return render_template('loading.html')
+
+global status
+status = 0
+
+
+def wrap_fun(query, minimum_date, jurisdiction):
+    global status
+
     # Search Query
-    query = request.args.get('search')
+
     # Jurisdiction level ('Federal' or state abbreviation)
-    jurisdiction = request.args.get('state')
-    minimum_date = request.args.get('earliestdate')
-    print(query)
-    print(jurisdiction)
-    print(minimum_date)
+
     output_message = ''
     if not query:
         res = []
         output_message = ''
         print('no query')
-        return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=res)
+        return project_name, net_id, output_message, res
+
     else:
         # =====Reddit cos processing START=========
         # title, id, selftext, url, created_utc e60m7
@@ -62,6 +78,9 @@ def search():
         index_to_posts_id = {index: post_id for index,
                              post_id in enumerate(data)}
         print('created index')
+
+        status = 10
+
         n_feats = 5000
         # doc_by_vocab = np.empty([len(data)+1, n_feats])
         print('initialize numpy array')
@@ -72,6 +91,8 @@ def search():
                                     norm='l2')
         print("initialize vectorizer")
 
+        status = 20
+
         # d_array = [str(data[d]['selftext'])+str(data[d]['title']) for d in data]
         d_array = []
         for d in data:
@@ -79,10 +100,14 @@ def search():
             d_array.append(s)
 
         print("built d_array")
+
+        status = 30
+
         d_array.append(query)
         print("concatenated text and query")
         doc_by_vocab = tfidf_vec.fit_transform(d_array).toarray()
         print('to array')
+        status = 40
         sim_posts = []
         for post_index in range(num_posts):
             # score = get_sim(doc_by_vocab[post_index], doc_by_vocab[num_posts])
@@ -92,10 +117,15 @@ def search():
             den = np.multiply(np.sqrt(q_vector.dot(q_vector)),
                               np.sqrt(post_vector.dot(post_vector)))
             score = num/den
+            if np.isnan(score):
+                score = 0
             sim_posts.append((score, post_index))
         print('calculated similarities')
         sim_posts.sort(key=lambda x: x[0], reverse=True)
         print('sorted similarities')
+
+        status = 50
+
         res = []
         for k in range(10):
             e = data[index_to_posts_id[sim_posts[k][1]]]
@@ -106,6 +136,9 @@ def search():
         print('retrieved reddit cases')
         # =====CaseLaw Retrieval=====
         print('begin caselaw retrieval')
+
+        status = 60
+
         caselaw, debug_msg = rank_cases(
             query, jurisdiction=jurisdiction, earlydate=minimum_date)
         error = False
@@ -130,23 +163,71 @@ def search():
                     case['case_summary'] = case['case_summary'] + '...'
         # =====Processing results================
         print('completed caselaw retrieval')
+
+        status = 70
+
         for i in range(5):
             post = res[i]
             if (post['selftext'] is not None) and (len(post['selftext'])) > 500:
                 post['selftext'] = post['selftext'][0:500] + '...'
 
         caselaw_message = "Historical precedences:"
+
+        status = 80
+
         output_message = "Past discussions:"
         print('rendering template..')
+
+        status = 100
         # ============================
 
-        return render_template('search.html', name=project_name, netid=net_id,
-                               output_message=output_message, data=res[:5], casedata=caseresults,
-                               caselaw_message=caselaw_message,
-                               user_query=query, debug_message=debug_msg,
-                               is_error=error)
+        return project_name, net_id, output_message, res[:5], caseresults, caselaw_message, query, debug_msg, error
+
+
+@irsystem.route('/', methods=['GET'])
+def search():
+    return render_template('search.html')
 
 
 @irsystem.route('/about', methods=['GET'])
 def about():
     return render_template('about.html')
+
+
+@irsystem.route("/results/<job_key>", methods=['GET'])
+def get_results(job_key):
+    job = Job.fetch(job_key, connection=conn)
+    if job.is_finished:
+        return jsonify(job.result), 200
+    else:
+        return "Nay!", 202
+
+
+@irsystem.route('/progress')
+def progress():
+    global status
+
+    def generate():
+        global status
+        while status <= 100:
+            yield "data:" + str(status) + "\n\n"
+            time.sleep(0.5)
+
+    return Response(generate(), mimetype='text/event-stream')
+
+
+@irsystem.route('/start', methods=['POST'])
+def get_counts():
+
+    data = json.loads(request.data.decode())
+    data = data['data']
+    print(data)
+    query = data[0]
+    min_date = data[1]
+    state = data[2]
+
+    job = q.enqueue_call(
+        func=wrap_fun, args=(query, min_date,
+                             state), result_ttl=5000
+    )
+    return job.get_id()
---
+++ app/static/main.js
@@ -0,0 +1,106 @@
+(function () {
+
+  'use strict';
+
+  angular.module('WordcountApp', [])
+
+    .controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
+      function ($scope, $log, $http, $timeout) {
+
+        $scope.submitButtonText = 'Search';
+        $scope.loading = false;
+        $scope.urlerror = false;
+        $scope.finished = false;
+        $scope.reddit_data = []
+
+        $scope.getResults = function () {
+          // get the URL from the input
+          var userInput = $scope.url;
+          var min_date = $scope.min_date
+          var state = $scope.state
+          // fire the API request
+          $http.post('/start', { 'data': [userInput, min_date, state] }).
+            success(function (results) {
+              $log.log(results);
+              getWordCount(results);
+              $scope.wordcounts = null;
+              $scope.loading = true;
+              $scope.submitButtonText = 'Loading...';
+              $scope.urlerror = false;
+            }).
+            error(function (error) {
+              $log.log(error);
+            });
+
+        };
+
+        function getWordCount(jobID) {
+
+          var timeout = '';
+
+          var poller = function () {
+            // fire another request
+            $http.get('/results/' + jobID).
+              success(function (data, status, headers, config) {
+                if (status === 202) {
+                  $log.log(data, status);
+                } else if (status === 200) {
+                  $log.log(data);
+                  $scope.reddit_data = data[3]
+                  $scope.caselaw_data = data[4]
+                  $scope.loading = false;
+                  $scope.submitButtonText = "Search";
+                  $scope.wordcounts = data;
+                  $timeout.cancel(timeout);
+                  $scope.finished = true;
+                  return false;
+                }
+                // continue to call the poller() function every 2 seconds
+                // until the timeout is cancelled
+                timeout = $timeout(poller, 2000);
+              }).
+              error(function (error) {
+                $log.log(error);
+                $scope.loading = false;
+                $scope.submitButtonText = "Search";
+                $scope.urlerror = true;
+              });
+          };
+
+          poller();
+
+        }
+
+      }])
+
+    .directive('wordCountChart', ['$parse', function ($parse) {
+      return {
+        restrict: 'E',
+        replace: true,
+        template: '<div id="chart"></div>',
+        link: function (scope) {
+          scope.$watch('wordcounts', function () {
+            d3.select('#chart').selectAll('*').remove();
+            var data = scope.wordcounts;
+            for (var word in data) {
+              var key = data[word][0];
+              var value = data[word][1];
+              d3.select('#chart')
+                .append('div')
+                .selectAll('div')
+                .data(word)
+                .enter()
+                .append('div')
+                .style('width', function () {
+                  return (value * 3) + 'px';
+                })
+                .text(function (d) {
+                  return key;
+                });
+            }
+          }, true);
+        }
+      };
+    }]);
+
+}());
