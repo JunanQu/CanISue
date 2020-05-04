5bd610abb45abcca33e406487d3968ca588841bb
Nikhil Saggi <ns739@cornell.edu>
2020-05-04 01:43:52 -0400

    clean up code

--- app/irsystem/controllers/case_ranking.py
+++ app/irsystem/controllers/case_ranking.py
@@ -73,13 +73,17 @@ def rank_cases(query:str, stem_tokens=False, jurisdiction='', earlydate = '', nc
     between the query and the full case texts. Requires unmetered API key.
 
     Parameters:
-    query: the query
-    stem_tokens: if True, removes token stemmings before computing tf-idf matrix
-    (False by default because it's a bottleneck and it does not affect results much)
+     - query: the query
+     - stem_tokens: if True, removes token stemmings before computing tf-idf matrix
+       (False by default because it's a bottleneck and it does not affect results much)
+     - jurisdiction: if specified, query cases only under certain jurisdiction
+     - earlydate: if specified, query cases only after certain date
+     - ncases: top number of matches to retrieve from similarity model
 
-    Returns:
-     - ranked list of dicts with fields case_name <str>, case_summary <str>, and score <float>
-     - None if API request fails
+
+    Returns: <tuple> of 
+     - ranked list of dicts with relevant case fields (<None> if API fails)
+     - debug message indicating error
     """
 
     ## ==== <STEP 1: load cases> ==== ##
@@ -97,13 +101,11 @@ def rank_cases(query:str, stem_tokens=False, jurisdiction='', earlydate = '', nc
         response = utils.get_request_caselaw(url).json()
         cases = response['results']
         
-        i = 1 # limit to 5 requests (500 cases) because that should be more than enough
+        i = 1 # limit to 2 requests (200 cases) because that should be more than enough
         while response['next'] and i < 2: 
             response = utils.get_request_caselaw(response['next']).json()
             cases.extend(response['results'])
             i += 1
-        # cases=cases[:250]
-        print(len(cases))
     except Exception:
         print("API request failed")
         debug_message = 'The Case Law API is currently down. Sorry for the inconvenience!'
@@ -165,6 +167,7 @@ def rank_cases(query:str, stem_tokens=False, jurisdiction='', earlydate = '', nc
         case_jurisdictions.append(court_types[closest_court])    
 
     ## ==== <STEP 3: determine opinion of cases> ==== ##
+
     case_outcomes = []
 
     trial_regex = re.compile("(verdict|judgment)[ \w]* (plaintiff|defendant)", re.IGNORECASE)
@@ -252,10 +255,14 @@ def rank_cases(query:str, stem_tokens=False, jurisdiction='', earlydate = '', nc
             if appeal_outcomes[-1] == "negative":
                 case_outcomes[-1] = ("plaintiff" if case_outcomes[-1].lower() == "defendant" else "defendant")
 
+    ## ==== <STEP 4: rank cases by similarity to query> ==== ##
+
     documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(case_texts)]
     model = Doc2Vec(documents, vector_size=5, window=2, min_count=1, workers=4)
     updated_query = model.infer_vector([query])
     sims = model.docvecs.most_similar([updated_query], topn=ncases)
+
+    ## ==== <STEP 5: summarize top cases> ==== ##
 
     case_summaries = [""]*len(cases)
     for idx,_ in sims:
@@ -272,64 +279,22 @@ def rank_cases(query:str, stem_tokens=False, jurisdiction='', earlydate = '', nc
 
         case_summaries[idx] = ". ".join([sent for sent,valid in case_sents.items() if valid])
 
+    ## ==== <STEP 6: return> ==== ##
+
     out_cases = []
-    # for doc in sims:
-    #     print(documents[doc[0]])
-    # out_cases = dict()
     for i in sims:
         case_dict = dict()
         case_dict.update({
-            'case_name':case_names[i[0]],
-            'case_summary':case_summaries[i[0]],
-            'case_url':case_urls[i[0]],
-            'case_outcome':case_outcomes[i[0]],
-            'score': (i[1] + 1) / 2
+            'case_name'    : case_names[i[0]],
+            'case_summary' : case_summaries[i[0]],
+            'case_url'     : case_urls[i[0]],
+            'case_outcome' : case_outcomes[i[0]],
+            'score'        : (i[1] + 1) / 2
         })
         out_cases.append(case_dict)
+        
     return (out_cases, debug_message)
         
-        
-    # case_opinions = []
-    # for opinions in [case['casebody']['data']['opinions'] for case in cases]:
-    #     for opinion in opinions:
-    #         if opinion['type'] == "majority":
-    #             case_opinions.append(opinion['text'].replace("\n", " "))
-    #             break
-
-    ## STEP 3: assign similarity scores to cases ##
-
-    # compute tf-idf scores
-    # if stem_tokens:
-    #     stemmer = PorterStemmer()
-    #     vec = TfidfVectorizer(tokenizer=tokenize, 
-    #                     min_df=.01, 
-    #                     max_df=0.8, 
-    #                     max_features=5000, 
-    #                     stop_words=[stemmer.stem(item) for item in ENGLISH_STOP_WORDS], 
-    #                     norm='l2')
-    # else:
-    #     vec = TfidfVectorizer(min_df=.01, 
-    #                     max_df=0.8, 
-    #                     max_features=5000, 
-    #                     stop_words='english', 
-    #                     norm='l2')
-    # try:
-    #     # compute cosine similarity of cases to search query
-    #     tfidf_matrix = vec.fit_transform(case_texts + [query]).toarray()
-    #     query_vec = tfidf_matrix[-1]
-    #     scores = [cosine_similarity(query_vec.reshape(1,-1), doc_vec.reshape(1,-1))[0][0] for doc_vec in tfidf_matrix[:-1]]
-
-    #     ## STEP 4: sort and return cases ##
-
-    #     results = pd.DataFrame(list(zip(case_names, case_texts, case_urls, scores)), columns=['case_name', 'case_summary', 'case_url', 'score'])
-    #     results = results.sort_values('score', ascending=False).reset_index(drop=True)
-        
-    #     return (results.to_dict('records'), debug_message)
-    # except:
-    #     debug_message = 'No cases found. Please enter a new query or try a wider date range!'
-    #     return (None, debug_message)
-
-    
 
 if __name__ == "__main__":
     rank_cases("fence built on my property")
